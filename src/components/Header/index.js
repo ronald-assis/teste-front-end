7a5327b7f68e1f1f61115094b39c549a9d4d0b16
Ronald Assis <ronaldassis71@gmail.com>
2022-01-19 22:27:47 -0300

    created Navigation component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import Benefits from '../Benefits';
 import LogoAndUser from '../LogoAndUser';
+import Navigation from '../Navigation';
 
 const Header = () => {
   return (
     <header>
       <Benefits />
       <LogoAndUser />
+      <Navigation />
     </header>
   );
 };
