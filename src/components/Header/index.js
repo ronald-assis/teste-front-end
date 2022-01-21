0d2cb4f26037712d055c092b53c9d8e11e482e1b
Ronald Assis <ronaldassis71@gmail.com>
2022-01-21 06:14:18 -0300

    function to the  button see more

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import Benefits from '../Benefits';
 import LogoAndUser from '../LogoAndUser';
 import Navigation from '../Navigation';
+import ShoppingCart from '../ShoppingCart';
+import './Header.scss';
 
 const Header = () => {
   return (
     <header>
       <Benefits />
+      <ShoppingCart />
       <LogoAndUser />
       <Navigation />
     </header>
--- src/data.js
+++ src/data.js
@@ -1,105 +1,107 @@
-const data = {
-  success: true,
-  products: [
-    {
-      productName: 'Bateria Eletrônica',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-bateria-eletronica.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Kit de Pratos',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-kit-pratos.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Pedal Duplo para Bumbo',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-pedal-duplo-para-dumbo.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Módulo para Bateria',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-modulo-para-bateria.png',
-      price: 1499.9,
-    },
+const data = () => {
+  return {
+    success: true,
+    products: [
+      {
+        productName: 'Bateria Eletrônica',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-bateria-eletronica.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Kit de Pratos',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-kit-pratos.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Pedal Duplo para Bumbo',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-pedal-duplo-para-dumbo.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Módulo para Bateria',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-modulo-para-bateria.png',
+        price: 1499.9,
+      },
 
-    {
-      productName: 'Prato 17” Medium Crash',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-prato-17-medium-crash.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Kit Bag para Tambores',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-kit-bag-para-tambores.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Banco para Bateria',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-banco-para-bateria.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Pedestal para Percussão',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-pedestal-para-percussao.png',
-      price: 1499.9,
-    },
+      {
+        productName: 'Prato 17” Medium Crash',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-prato-17-medium-crash.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Kit Bag para Tambores',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-kit-bag-para-tambores.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Banco para Bateria',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-banco-para-bateria.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Pedestal para Percussão',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-pedestal-para-percussao.png',
+        price: 1499.9,
+      },
 
-    {
-      productName: 'Mixer de Video',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-mixer-de-video.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Guitarra Jackson',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-guitarra-jackson.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Contrabaixo Elétrico',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-contrabaixo-eletrico.png',
-      price: 1499.9,
-    },
-    {
-      productName: 'Sistema de Microfone s/ Fio',
-      descriptionShort:
-        'Many desktop publishing packages and web page editors now Many',
-      photo:
-        'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-sistema-de-microfones-sem-fio.png',
-      price: 1499.9,
-    },
-  ],
+      {
+        productName: 'Mixer de Video',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-mixer-de-video.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Guitarra Jackson',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-guitarra-jackson.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Contrabaixo Elétrico',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-contrabaixo-eletrico.png',
+        price: 1499.9,
+      },
+      {
+        productName: 'Sistema de Microfone s/ Fio',
+        descriptionShort:
+          'Many desktop publishing packages and web page editors now Many',
+        photo:
+          'http://econverse.digital/teste-front-end/junior/ninja-som/fotos-produtos/foto-sistema-de-microfones-sem-fio.png',
+        price: 1499.9,
+      },
+    ],
+  };
 };
 
 export default data;
